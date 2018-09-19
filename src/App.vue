dc5da9a7c3f6b12b67e573975e1421c623bd23a9
Michael Smith <msmithboise@gmail.com>
2018-09-19 09:30:54 -0600

    build

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+
+<template>
+  <div id="app">
+    <!-- <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/results">Results</router-link>
+    </div> -->
+    <router-view/>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data () {
+    return {
+      clipped: false,
+      drawer: true,
+      fixed: false,
+      items: [{
+        icon: 'bubble_chart',
+        title: 'Inspire'
+      }],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Vuetify.js'
+    }
+  }
+}
+</script>
